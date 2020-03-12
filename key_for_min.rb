d365416f7aeba569983c9590867a14109bee7a86
Celeste Thomas <celestesmthomas@gmail.com>
2020-03-12 18:26:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
      smallest_value = value
     smallest_key = key
     else
-      return smallest_value
+      return smallest_key
     end
   end
   smallest_key
