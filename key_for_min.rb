175871874a2d01753080abd4c773a5708ddbef70
Celeste Thomas <celestesmthomas@gmail.com>
2020-03-12 18:19:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,12 @@
 
 def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
-    if name_hash[key]
-      n
-  
-
+    x = nil
+    if value < nil && value > value 
+      x = value
+      puts x
+    else
+      puts x 
+    end
+  end
 end
